0faa2fe4d7c1f4b1edc64a5332abf5f91e1cc4b9
Sean Thomas <sean.alan.thomas@gmail.com>
2016-07-01 16:58:46 -0600

    fix proxies

--- app/controllers/finder/google.controller.js
+++ app/controllers/finder/google.controller.js
@@ -1,6 +1,7 @@
 var google = require('../../google/google');
 var Bluebird = require('bluebird');
 var _ = require('lodash');
+var mppProxies = require('../../../config/mppProxies.js');
 
 google.resultsPerPage = 10;
 
@@ -11,27 +12,57 @@ function findHref(links){
   return links[0].href;
 }
 
+function getProxy(){
+  //158.222.4.208:80:test_3:dog
+  //209.222.100.171:80:test_3:dog
+  /*var ip = "209.222.100.171";
+   var port = 80;
+   var username = "test_3";
+   var password = "dog";*/
+
+  return mppProxies[Math.floor((Math.random() * 5))]; //out of 6 proxies
+}
+
 function findCompanyWebsite(companyName) {
   var deferred = Bluebird.pending();
-
-  var ip = "37.48.125.203";
-  var port = 222;
-  var username = "bryson";
-  var password = "Wayne1!!!";
+  var proxy = getProxy();
 
 
-  google(username, password, ip, port, companyName, function (err, res) {
-    if (err) {
-      deferred.reject(err);
-    } else if(res.links.length < 1) {
-      deferred.reject('Could not find domain name for this company in google');
-    } else {
-      href = findHref(res.links);
-      deferred.resolve(href);
+
+  var ip = proxy.ip;
+  var port = proxy.port;
+  var username = proxy.username;
+  var password = proxy.password;
+
+  var retry = 0;
+  return tryGoogle(companyName, retry).catch(function() {
+    retry += 1;
+    if (retry < 3) {
+      return tryGoogle(companyName, retry);
     }
-  });
-  return deferred.promise;
+  })
+
+
 }
+
+function tryGoogle(companyName){
+  return new Bluebird(function(resolve, reject){
+    var proxy = getProxy();
+    google(proxy.username, proxy.password, proxy.ip, proxy.port, companyName, function (err, res) {
+      if (err) {
+        reject(err);
+      } else if (res.links.length < 1) {
+        reject('Could not find domain name for this company in google');
+      } else {
+        href = findHref(res.links);
+        console.log(href)
+        resolve(href);
+      }
+    });
+  });
+}
+
+findCompanyWebsite("amplitude");
 
 exports.findCompanyWebsite = findCompanyWebsite;
 
---
+++ config/mppProxies.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    ip: "23.106.208.186",
+    port: 29842,
+    username: "sthoma",
+    password: "TnVDp4Zy"
+  }, {
+    ip: "23.106.26.113",
+    port: 29842,
+    username: "sthoma01",
+    password: "2cM3mFpv"
+  }, {
+    ip: "23.106.26.239",
+    port: 29842,
+    username: "sthoma01",
+    password: "2cM3mFpv"
+  }, {
+    ip: "23.82.86.163",
+    port: 29842,
+    username: "sthoma01",
+    password: "2cM3mFpv"
+  }, {
+    ip: "23.82.86.49",
+    port: 29842,
+    username: "sthoma01",
+    password: "2cM3mFpv"
+  }, {
+    ip: "23.82.86.85",
+    port: 29842,
+    username: "sthoma01",
+    password: "2cM3mFpv"
+  }
+];
