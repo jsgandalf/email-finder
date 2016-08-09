976016519b4f858365bbb8d844807d585415fa78
Sean Thomas <sean.alan.thomas@gmail.com>
2016-08-09 10:39:49 -0600

    m

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -2,6 +2,7 @@ var express = require('express'),
   router = express.Router(),
   FinderCtrl = require('./finder/finder.controller'),
   SearchCtrl = require('./search/search.controller'),
+  ScrapeCtrl = require('./scrape/scrape.controller'),
   auth = require('./components/auth/auth.service');
 
 module.exports = function (app) {
@@ -11,6 +12,8 @@ module.exports = function (app) {
 router.get('/v1/guess', auth.isAuthenticated(), FinderCtrl.index);
 
 router.post('/v1/search', auth.isAuthenticatedSearch(), SearchCtrl.index);
+
+router.post('/v1/route', auth.isAuthenticatedSearch(), ScrapeCtrl.index);
 
 //http://localhost:3000/api/v1/guess?key=UZE6pY5Yz6z3ektV:NEgYhceNtJaee3ga:H5TYvG57F2dzJF7Ginvalidate&first=john&last=Johnson&domain=premera.com
 
---
+++ app/controllers/scrape/scrape.controller.js
@@ -0,0 +1,61 @@
+var google = require('../../google/scrape');
+var Bluebird = require('bluebird');
+var cancelSome = require('../../utils/cancel-some');
+
+function tryScrape(url, proxy){
+  return new Bluebird(function(resolve, reject, onCancel){
+    var aborter = google(proxy.username, proxy.password, proxy.ip, proxy.port, url, function (err, res) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+    onCancel(aborter);
+  });
+}
+
+function getResults(query, start, retry) {
+  if(typeof retry == 'undefined' || retry == null){
+    retry = 0;
+  }
+  var proxy = {
+    ip: "108.59.14.208",
+    port: 13010,
+    username: null,
+    password: null
+  };
+  return cancelSome.getFirst([
+    tryScrape(query,proxy),
+    tryScrape(query,proxy),
+    tryScrape(query,proxy)
+  ]).then(function (data) {
+    return data;
+  }).catch(function(err) {
+    retry += 1;
+    if (retry < 2) {
+      return getResults(query, start, retry);
+    }
+  });
+}
+
+//var query = 'site:www.linkedin.com/in/* OR site:www.linkedin.com/pub/* -inurl:"dir/+" -intitle:"profiles" "Real estate agent"';
+
+var proxy = {
+  ip: "108.59.14.208",
+  port: 13010,
+  username: null,
+  password: null
+};
+
+//tryGoogle(query,proxy);
+
+exports.index = function(req, res){
+  console.log(req.body.url)
+  return getResults("www.linkedin.com/in/realtor-real-estate-agent-samii-6121165a").then(function(data){
+    return res.json(data);
+  }).catch(function(err){
+    console.log(err);
+    return res.status(500).json(err);
+  });
+};
