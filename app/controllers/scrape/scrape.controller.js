6902195dc9c6807876288ced8d5c05e42074e421
Sean Thomas <sean.alan.thomas@gmail.com>
2016-08-11 11:53:58 -0600

    m

--- app/controllers/scrape/scrape.controller.js
+++ app/controllers/scrape/scrape.controller.js
@@ -3,55 +3,6 @@ var Bluebird = require('bluebird');
 var cancelSome = require('../../utils/cancel-some');
 var phantom = require('phantom');
 
-function tryScrape(url, proxy){
-  return new Bluebird(function(resolve, reject, onCancel){
-    var aborter = scrape(proxy.username, proxy.password, proxy.ip, proxy.port, url, function (err, res) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
-      }
-    });
-    onCancel(aborter);
-  });
-}
-
-function getResults(url, retry) {
-  if(typeof retry == 'undefined' || retry == null){
-    retry = 0;
-  }
-  var proxy = {
-    ip: "108.59.14.208",
-    port: 13010,
-    username: null,
-    password: null
-  };
-  return cancelSome.getFirst([
-    tryScrape(url,proxy),
-    tryScrape(url,proxy),
-    tryScrape(url,proxy)
-  ]).then(function (data) {
-    console.log(data);
-    return data;
-  }).catch(function(err) {
-    retry += 1;
-    if (retry < 2) {
-      return getResults(url, retry);
-    }
-  });
-}
-
-//var query = 'site:www.linkedin.com/in/* OR site:www.linkedin.com/pub/* -inurl:"dir/+" -intitle:"profiles" "Real estate agent"';
-
-var proxy = {
-  ip: "108.59.14.208",
-  port: 13010,
-  username: null,
-  password: null
-};
-
-//tryGoogle(query,proxy);
-
 function getProfileHtml(url){
   var sitepage = null;
   var phInstance = null;
@@ -84,8 +35,10 @@ function getProfileHtml(url){
 }
 
 exports.index = function(req, res){
-  return getProfileHtml(req.body.url).then(function(data){
-    console.log(data)
+  return getProfileHtml(req.body.url).then(function(data) {
+    return scrape(data)
+  }).then(function(data){
+    console.log(data);
     return res.json(data);
   }).catch(function(err){
     console.log(err);
