d804f985a9be9acb5a7d3d663e85af1c5e0a1d5d
Sean Thomas <sean.alan.thomas@gmail.com>
2016-08-11 11:23:23 -0600

    m

--- app/controllers/scrape/scrape.controller.js
+++ app/controllers/scrape/scrape.controller.js
@@ -51,8 +51,39 @@ var proxy = {
 
 //tryGoogle(query,proxy);
 
+function getProfileHtml(url){
+  var sitepage = null;
+  var phInstance = null;
+
+  return phantom
+    .create(["--proxy=108.59.14.208:15124", "--proxy-type=http"])
+    .then(function(instance) {
+      phInstance = instance;
+      return instance.createPage();
+    })
+    .then(function(page) {
+      sitepage = page;
+      page.setting('userAgent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36');
+      return page.open(url);
+    })
+    .then(function(status) {
+      console.log(status)
+      return sitepage.property('content');
+    })
+    .then(function (body) {
+      sitepage.close();
+      phInstance.exit();
+      return body;
+    })
+    .catch(function(err) {
+      console.log(err);
+      phInstance.exit();
+      throw err;
+    });
+}
+
 exports.index = function(req, res){
-  return getResults(req.body.url).then(function(data){
+  return getProfileHtml(req.body.url).then(function(data){
     console.log(data)
     return res.json(data);
   }).catch(function(err){
