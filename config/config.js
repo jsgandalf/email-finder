fdf50adf7835f7f28735918091a3ef8d681910b7
Sean Thomas <sean.alan.thomas@gmail.com>
2017-05-18 21:57:50 -0600

    fixes to database

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ var config = {
       name: 'messagesumo-email'
     },
     port: 3000,
-    db: 'mongodb://adbrocks99:a14AbbJJ9dlkUi023jlsdf@candidate.53.mongolayer.com:10223,candidate.52.mongolayer.com:10634/messagesumo-email?replicaSet=set-560ded97f3ea5ebdda00018d'
+    db: 'mongodb://adbrocks99afsdasdf:a14AfffbbJJ9dlkUi023jlsdf@candidate.4.mongolayer.com:11502,candidate.7.mongolayer.com:10620/messagesumo-email?replicaSet=set-591e662f21cc157adc000826'
   },
 
   test: {
@@ -19,7 +19,7 @@ var config = {
       name: 'messagesumo-email'
     },
     port: 3000,
-    db: 'mongodb://adbrocks99:a14AbbJJ9dlkUi023jlsdf@candidate.53.mongolayer.com:10223,candidate.52.mongolayer.com:10634/messagesumo-email?replicaSet=set-560ded97f3ea5ebdda00018d'
+    db: 'mongodb://adbrocks99afsdasdf:a14AfffbbJJ9dlkUi023jlsdf@candidate.4.mongolayer.com:11502,candidate.7.mongolayer.com:10620/messagesumo-email?replicaSet=set-591e662f21cc157adc000826'
   },
 
   production: {
@@ -28,7 +28,7 @@ var config = {
       name: 'messagesumo-email'
     },
     port: 3000,
-    db: 'mongodb://adbrocks99:a14AbbJJ9dlkUi023jlsdf@candidate.53.mongolayer.com:10223,candidate.52.mongolayer.com:10634/messagesumo-email?replicaSet=set-560ded97f3ea5ebdda00018d'
+    db: 'mongodb://adbrocks99afsdasdf:a14AfffbbJJ9dlkUi023jlsdf@candidate.4.mongolayer.com:11502,candidate.7.mongolayer.com:10620/messagesumo-email?replicaSet=set-591e662f21cc157adc000826'
   }
 };
 //mongo candidate.52.mongolayer.com:10634/messagesumo-email -u adbrocks99 -pa14AbbJJ9dlkUi023jlsdf
